4f93fdab972eb470ebe6297697105fe5f5445111
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-01-15 18:04:37 -0300

    pagination

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,13 +1,13 @@
 package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable pagincacao);
     // Se tiver ambiguidade de nome, use Curso_Nome
 
     // Caso não siga o padrão de nome do JPA, utiliza-se:
