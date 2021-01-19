948bf872a0e2a8309c77a458c3781729303e0d8a
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-01-19 10:54:15 -0300

    token auth 02

--- forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
 import br.com.alura.forum.config.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +26,13 @@ public class AutenticacaoController {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form) {
+    public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
         UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 
         try {
             Authentication authentication = authManager.authenticate(dadosLogin);
             String token = tokenService.gerarToken(authentication);
-            System.out.println(token);
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok(new TokenDto(token, "Bearer"));
         } catch (AuthenticationException e) {
             return ResponseEntity.badRequest().build();
         }
